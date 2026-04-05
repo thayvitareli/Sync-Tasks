70d5c2b4e2b1a65ea65a947735895eafd1572584
thayvitareli <thainyvitareli@hotmail.com>
2026-04-05 13:05:50 -0300

    feat: TaskRepository

---
+++ src/model/task/taskRepository.ts
@@ -0,0 +1,10 @@
+import { Task } from "./task"
+
+export interface TaskRepository {
+  create(task: Task): void
+  getAll(): Task[]
+  update(task: Task): void
+  delete(id: string): void
+  getUnsynced(): Task[]
+  markAsSynced(id: string): void
+}
