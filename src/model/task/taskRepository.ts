f155745b132366fc751aae0f021c0b3ca421919a
thayvitareli <thainyvitareli@hotmail.com>
2026-04-05 14:56:38 -0300

    feat: add getAllForSync and upsertFromSync method on TaskRepository

--- src/model/task/taskRepository.ts
+++ src/model/task/taskRepository.ts
@@ -3,8 +3,10 @@ import { Task } from "./task"
 export interface TaskRepository {
   create(task: Task): void
   getAll(): Task[]
+  getAllForSync(): Task[]
   update(task: Task): void
   delete(id: string): void
   getUnsynced(): Task[]
   markAsSynced(id: string): void
+  upsertFromSync(task: Task): void
 }
