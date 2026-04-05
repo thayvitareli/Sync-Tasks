931522bf10b7975f6846f0c87cac0e2554850b8c
thayvitareli <thainyvitareli@hotmail.com>
2026-04-05 15:36:15 -0300

    refactor: style for TaskCard component

--- src/components/taskCard.tsx
+++ src/components/taskCard.tsx
@@ -11,19 +11,21 @@ interface TaskCardProps {
 
 export default function TaskCard({task, handleToggleTask, handleDeleteTask}: TaskCardProps) {
   return (
-    <View className="flex-row bg-white w-full p-5 rounded-md justify-between">
-      <View className="flex-row items-center gap-2">
+    <View className="flex-row bg-[#151221] border border-[#2e264a] w-full p-4 rounded-xl items-center justify-between mb-3">
+      <View className="flex-row items-center flex-1 pr-4">
         <Pressable
           onPress={() => handleToggleTask(task.id)}
-          className={`w-6 h-6 flex items-center justify-center border border-primary rounded-sm ${task.completed ? "bg-primary" : ""}`}
+          className={`w-6 h-6 flex items-center justify-center rounded-md mr-3 border ${task.completed ? "bg-[#8e84f5] border-[#8e84f5]" : "border-[#7b719d] bg-transparent"}`}
         >
-          {task.completed ? <Ionicons name="checkmark" size={15} color="white" /> : null}
+          {task.completed && <Ionicons name="checkmark" size={16} color="white" />}
         </Pressable>
-        <Text className={`text-on-surface text-xl font-bold ${task.completed ? "line-through" : ""}`}>{task.title}</Text>
+        <Text className={`text-base font-semibold flex-1 ${task.completed ? "text-[#7b719d] line-through" : "text-[#ebe9f0]"}`}>
+          {task.title}
+        </Text>
       </View>
 
-      <Pressable onPress={() => {handleDeleteTask(task.id)}}>
-        <Ionicons name="trash" size={24} color="#4F46E5" />
+      <Pressable onPress={() => {handleDeleteTask(task.id)}} className="p-2">
+        <Ionicons name="trash-outline" size={20} color="#7b719d" />
       </Pressable>
     </View>
   )
